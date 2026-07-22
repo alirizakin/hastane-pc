83bbd1d31475ec61323436758bcdfba6f4a3bcdc
Ali Rıza <alirizakin@github.com>
2026-07-22 16:08:08 +0300

    Window 763x750, transparent arma logo PNG

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(779, 708);
+  Win32Window::Size size(763, 750);
   if (!window.Create(L"ISH Asistan", origin, size)) {
     return EXIT_FAILURE;
   }
